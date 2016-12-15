0327fb72eb3137ed0772accab68bec9148e785cd
Josua Schmid <josua.schmid@renuo.ch>
2016-12-15 08:51:31 +0100

    Initial setup

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Resthauer
+  class Application < Rails::Application
+    config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+  end
+end
