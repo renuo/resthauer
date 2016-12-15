4a01162186ddaf06cd3e819637fe84e24070a17d
Josua Schmid <josua.schmid@renuo.ch>
2016-12-15 09:43:23 +0100

    Add relations

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,4 @@
 class Menu < ApplicationRecord
+  has_many :ratings
+  has_many :users, through: :ratings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :ratings
+  has_many :menus, through: :ratings
 end
